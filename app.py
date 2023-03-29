ca89482c65a11166a0a55553daf3b063fbf70563
captainsalazar <57871522+syedalizain033@users.noreply.github.com>
2023-03-30 03:06:38 +0500

    Update the path

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pydeck as pdk
 import plotly.express as px
 
 DATA_URL=(
-"/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
+"Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 st.title("Motor Vehicle Collision in NewYork City")
 st.markdown("This application is a Steamlit dashboard that can be used to analyze the modern vehicles collision in NYC")
